23f4b7b9cae944283109784c02405190de43f39d
Kareem Yacoub <kyacou2@uic.edu>
2019-03-12 12:12:35 -0500

    added line ripple, resized and aligned button

--- init.js
+++ init.js
@@ -2,5 +2,4 @@ $(window).on('load', function() {
     window.mdc.autoInit();
 
     mdc.ripple.MDCRipple.attachTo($('#search-button')[0]);
-    mdc.ripple.MDCRipple.attachTo($('#search-text-field')[0]);
 });
