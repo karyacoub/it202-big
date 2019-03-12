650e3355f0943c962dccc4a29d6ad63e3fb7138d
Kareem Yacoub <kyacou2@uic.edu>
2019-03-12 14:15:37 -0500

    set up geolocation

--- searchController.js
+++ searchController.js
@@ -1,3 +1,46 @@
+var url = "https://api.yelp.com/v3/businesses/search?latitude=41.8704428&longitude=-87.6485634&term=Starbucks";
+
 $(document).ready(function() {
-    console.log('search page ready');
+    $('#search-button').on('click', function() {
+        if(!isSearchEmpty())
+        {
+            // get current user's location
+            // Note: getLocation must take a callback function since getCurrentPosition() is asynchronous
+            getLocation(function(coordinates) { 
+                
+            });
+
+            // send Yelp Fusion API call
+
+
+            // load restauraunt list page
+            loadScreen('restaurant-list');
+        }
+    });
 })
+
+function isSearchEmpty()
+{
+    return $('#search-text-field').val().length <= 0;
+}
+
+function getLocation(callback)
+{
+    if ('geolocation' in navigator) 
+    {
+        // prompt user for location
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var coordinates = { latitude: position.coords.latitude, longitude: position.coords.longitude };
+            callback(coordinates);
+        },
+        function(error) {
+            // display snackbar with location retrieval error
+            console.log('Error retrieving location');
+        });
+    } 
+    else 
+    {
+        // display snackbar with location retrieval error
+        console.log('Error retrieving location');
+    }
+}
