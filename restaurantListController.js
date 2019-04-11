aedc247aa680449e6d15d37bcf4eaa1cfe1170a5
Kareem Yacoub <kyacou2@uic.edu>
2019-04-10 22:15:42 -0500

    removed unnecessary restaurantListController.js

--- restaurantListController.js
+++ restaurantListController.js
@@ -240,3 +240,26 @@ function getZomatoInfo(lat, lng, callback)
         }
     });
 }
+
+function militaryToStandardTime(time)
+{
+    var hours = parseInt(time.substring(0, 2));
+    var minutes = time.substring(2, 4);
+    var amOrPm = 'AM';
+
+    if(hours >= 12)
+    {
+        amOrPm = 'PM'
+        
+        if(hours > 12)
+        {
+            hours -= 11;
+        }
+    }
+    else if(hours === 0)
+    {
+        hours = 12;
+    }
+
+    return hours + ':' + minutes + ' ' + amOrPm;
+}
